a1a491ea34698e1982e3812c68c2c7eb4320ccb1
ChiriVulpes <github@chiri.works>
2025-07-29 14:43:37 +0000

    Deploy 421ba97: Remove Slot.use array inputs option

--- index.d.ts
+++ index.d.ts
@@ -1263,9 +1263,6 @@ declare module "kitsui/component/Slot" {
         else(initialiser: Slot.Initialiser): this;
     }
     export interface SlotExtensions {
-        use<const STATES extends State<any>[]>(states: STATES, initialiser: (slot: ComponentInsertionTransaction, ...values: {
-            [INDEX in keyof STATES]: STATES[INDEX] extends State<infer T> ? T : never;
-        }) => Slot.InitialiserReturn): this;
         use<T>(state: T | State<T>, initialiser: (slot: ComponentInsertionTransaction, value: T) => Slot.InitialiserReturn): this;
         if(state: State<boolean>, initialiser: Slot.Initialiser): this & SlotIfElseExtensions;
         preserveContents(): this;
